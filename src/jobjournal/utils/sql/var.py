edfd6f11283953254349b143abc536b2fce522ab
Florentin Creme <contact.fcreme@gmail.com>
2026-02-26 19:24:44 +0100

    feat(db): add mapping between class attributes and DB columns

---
+++ src/jobjournal/utils/sql/var.py
@@ -0,0 +1,27 @@
+class PositionsTable:
+    # Define database variables' name for positions table
+    table_pos ="positions"
+    
+    id = "id"
+    date = "pub_date"
+    title = "position"
+    comp = "company"
+    loc = "location"
+    source = "source"
+    interest = "hype"
+    status = "status"
+    salary = "salary"
+    details = "details"
+    skills = "skills"
+    motivation = "motivation"
+    timeline = "actions"
+
+class ContactsTable:
+    # Define database variables' name for contacts table
+    table_con = "contacts"
+    
+    id = "id"
+    name = "name"
+    comp = "company"
+    mail = "mail"
+    phone = "phone"
