380b3df88ad447de79127a470c11109a0eb96465
Florentin Creme <contact.fcreme@gmail.com>
2026-02-26 18:53:05 +0100

    chore(entrypoint): move env var checks to support logging setup
    Configure logging early in the entrypoint to initialize st.session_state.db_log before importing pages that use logging.
    While doing this, move env var validation from src/jobjournal/jobjournal_gui.py to entrypoint.py for better structure consistency.

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,62 @@ import warnings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
+import streamlit as st
+import sqlite3
+import logging
+import os
+
+# Configure log file for the operations on the database
+if "db_log" not in st.session_state:
+    st.session_state["db_log"] = "db.log"
+
+logging.basicConfig(
+    filename=st.session_state.db_log,
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(message)s",
+)
+
 from src.jobjournal.jobjournal_gui import main
 
 if __name__=="__main__":
+    # Check if environ. var. for db path has been defined
+    if "db_path" not in st.session_state:
+        if "JOBJOURNAL_DB_PATH" in os.environ:
+            st.session_state["db_path"] = os.environ["JOBJOURNAL_DB_PATH"]
+
+            # Check if the db exists, if not : create an empty database using the template
+            if not os.path.isfile(st.session_state.db_path):
+                st.toast("Création d'une nouvelle base donnée...")
+
+                db_templ = "src/jobjournal/utils/templ/db.init.sql"
+                if os.path.isfile(db_templ):
+                    with sqlite3.connect(st.session_state.db_path) as conn:
+                        with open(db_templ, "r", encoding="utf-8") as templ:
+                            conn.executescript(templ.read())
+
+                    st.toast("✅ Base de données crée avec succès.")
+                    logging.info("Database created successfully")
+
+                else:
+                    st.toast("❌ Template non disponible.")
+                    logging.info(f"Unable to find the database's template at {db_templ}")
+                    raise FileNotFoundError(f"{db_templ} not found.")
+
+        else:
+            st.markdown("# Job Seeker's Journal")
+            st.error("⚠️ La variable d'environnement JOBJOURNAL_DB_PATH n'existe pas.")
+            st.markdown(
+                "Pour utiliser l'application, définir l'emplacement de la base de données " \
+                "dans la variable d'environnement JOBJOURNAL_DB_PATH, puis relancer l'application."
+            )
+
+            st.code(
+                "$ export JOBJOURNAL_DB_PATH=/path/to/database\n" \
+                "$ pdm run streamlit run entrypoint.py", 
+                language="bash"
+            )
+
+            st.stop()
+
+    # Run app
     main()
--- src/jobjournal/jobjournal_gui.py
+++ src/jobjournal/jobjournal_gui.py
@@ -3,8 +3,6 @@ import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
 import streamlit as st
-import sqlite3
-import os
 
 from src.jobjournal.utils.pages.overview import overview
 from src.jobjournal.utils.pages.add_position import add_position
@@ -28,47 +26,9 @@ class MultiPageApp:
         page["function"]()
 
 def main():
-    # Check if environ. var. for db path has been defined
-    if "db_path" not in st.session_state:
-        if "JOBJOURNAL_DB_PATH" in os.environ:
-            st.session_state["db_path"] = os.environ["JOBJOURNAL_DB_PATH"]
+    app = MultiPageApp()
 
-            # Check if the db exists, if not : create an empty database using the template
-            if not os.path.isfile(st.session_state.db_path):
-                st.toast("Création d'une nouvelle base donnée...")
+    app.add_page("Vue d'ensemble", overview)
+    app.add_page("Ajouter une offre", add_position)
 
-                db_templ = "src/jobjournal/utils/templ/db.init.sql"
-                if os.path.isfile(db_templ):
-                    with sqlite3.connect(st.session_state.db_path) as conn:
-                        with open(db_templ, "r", encoding="utf-8") as templ:
-                            conn.executescript(templ.read())
-
-                    st.toast("✅ Base de données crée avec succès.")
-
-                else:
-                    st.toast("❌ Template non disponible.")
-                    raise FileNotFoundError(f"{db_templ} not found.")
-
-            # Execute the app
-            app = MultiPageApp()
-
-            app.add_page("Vue d'ensemble", overview)
-            app.add_page("Ajouter une offre", add_position)
-
-            app.run()
-
-        else:
-            st.markdown("# Job Seeker's Journal")
-            st.error("⚠️ La variable d'environnement JOBJOURNAL_DB_PATH n'existe pas.")
-            st.markdown(
-                "Pour utiliser l'application, définir l'emplacement de la base de données " \
-                "dans la variable d'environnement JOBJOURNAL_DB_PATH, puis relancer l'application."
-            )
-
-            st.code(
-                "$ export JOBJOURNAL_DB_PATH=/path/to/database\n" \
-                "$ pdm run streamlit run entrypoint.py", 
-                language="bash"
-            )
-
-            st.stop()
+    app.run()
